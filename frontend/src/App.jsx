b7753c92fc312b129f6d3b8e82c13bcdcbeeebb2
FMichel5173 <fmichel81@sfr.fr>
2023-01-09 13:26:33 +0100

    Create the VideoLibrary component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import Home from "./pages/Home";
-
+import VideoLibrary from "./components/VideoLibrary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <VideoLibrary />
     </div>
   );
 }
---
+++ frontend/src/components/VideoLibrary.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const videoLibrary = () => {
+  return (
+    <div>
+      <h1>Hello to video library</h1>
+    </div>
+  );
+};
+
+export default videoLibrary;
