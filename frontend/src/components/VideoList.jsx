8b2af4cff74d063e2d749cc8b764ad031b72b42e
FMichel5173 <fmichel81@sfr.fr>
2023-02-01 21:43:32 +0100

    Display of all videos from the database on VideoList in alphabetical order

--- frontend/src/components/VideoList.jsx
+++ frontend/src/components/VideoList.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 import VideoCard from "./VideoCard";
 
-function VideoList() {
+function VideoList({ videoList }) {
   return (
-    <section>
-      <ul>
-        <li className="VideoList">
-          <VideoCard />
-        </li>
+    <section className="listVideo">
+      <ul className="VideoList">
+        {videoList && videoList.length
+          ? videoList.map((video) => (
+              <li key={video.id}>
+                <VideoCard video={video} />
+              </li>
+            ))
+          : "Pas de DVD à afficher"}
       </ul>
     </section>
   );
 }
+
+VideoList.propTypes = {
+  videoList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default VideoList;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import VideoList from "../components/VideoList";
 import "../Home.css";
 
 function Home() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/videos`).then((response) => {
+      setVideos(response.data);
+    });
+  }, []);
+
+  const sortedVideos = videos.sort((a, b) => {
+    const titleA = a.title.toLowerCase();
+    const titleB = b.title.toLowerCase();
+    if (titleA < titleB) {
+      return -1;
+    }
+    if (titleA > titleB) {
+      return 1;
+    }
+    return 0;
+  });
+
   return (
     <div className="Home">
-      <VideoList />
+      <VideoList videoList={sortedVideos} />
     </div>
   );
 }
